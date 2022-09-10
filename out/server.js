266fb41e9d4267b48794c86ab40b42046776178b
Alexfanu <78355480+Alexfanu@users.noreply.github.com>
2022-09-10 17:56:00 +0300

    Added return of header

--- out/server.js
+++ out/server.js
@@ -1,2 +1,45 @@
 "use strict";
-console.log("successfull build");
+// import express from "express";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// const app = express
+// const port = 3000
+// express().get('/', (req: any, res: { send: (arg0: string) => void }) => {
+//   res.send('Hello World!')
+// })
+// express().listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+var cors = require("cors");
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(cors());
+const port = process.env.PORT || 5456;
+app.get('/', (req, res) => {
+    console.log(req.headers);
+    console.log(req.body);
+    res.status(200).send(
+    // {"ipaddress":req.socket.remoteAddress}
+    req.headers);
+});
+app.post('/', (req, res) => {
+    console.log(req.headers);
+    console.log(req.body);
+    res.send("Heres");
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+// import express, { Request, Response } from 'express';
+// const app = express();
+// const port = 3000;
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Hello World!');
+// });
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// });
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,85 @@
-console.log("successfull build")
+// import express from "express";
 
 
+// const app = express
+// const port = 3000
+
+
+
+// express().get('/', (req: any, res: { send: (arg0: string) => void }) => {
+//   res.send('Hello World!')
+// })
+
+
+// express().listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+
+import express, { Express, Request, Response } from 'express';
+
+
+
+
+const app = express();
+
+var cors = require("cors")
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+const port = process.env.PORT || 5456;
+
+app.get('/', (req: Request, res: Response) => {
+  
+  console.log( req.headers);
+  console.log( req.body);
+
+  res.status(200).send(
+
+    // {"ipaddress":req.socket.remoteAddress}
+    req.headers
+    )
+
+
+});
+
+app.post('/', (req: Request, res: Response) => {
+  
+  console.log( req.headers);
+  console.log( req.body);
+
+  res.send("Heres")
+
+
+});
+
+
+
+app.listen( port  , () => {  // || port
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+// import express, { Request, Response } from 'express';
+
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// });
